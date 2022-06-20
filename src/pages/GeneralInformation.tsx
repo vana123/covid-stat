72dd464bf42d04121a3caf122abb65673525bcdc
Vaana <35535603+vana123@users.noreply.github.com>
2022-06-20 09:44:58 +0300

    set default value on page

--- src/pages/GeneralInformation.tsx
+++ src/pages/GeneralInformation.tsx
@@ -5,6 +5,7 @@ import { useSelector } from "react-redux";
 import { Charts } from "../components/Charts";
 import { countrySlise } from "../store/reducers/Country";
 import { statSlise } from "../store/reducers/stat";
+import { dateSlise } from "../store/reducers/Date";
 
 export const GeneralInformation = () => {
 	const dispatch = useAppDispatch();
@@ -12,23 +13,18 @@ export const GeneralInformation = () => {
 	const { isLoading, error, data } = useAppSelector(
 		(state) => state.statReducer
 	);
+	const { date } = useAppSelector((store) => store.dateReducer);
 
 	useEffect(() => {
 		const today = new Date();
-		console.log(
-			`daily/${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`
-		);
 		dispatch(
-			fetchStat(
-				`daily/${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`
+			dateSlise.actions.chenge(
+				`${today.getFullYear()}-${today.getMonth()}-${today.getDay()}`
 			)
 		);
-		dispatch(countrySlise.actions.chenge);
+		dispatch(fetchStat(`daily/${date}}`));
+		dispatch(countrySlise.actions.chenge(""));
 	}, []);
-
-	useEffect(() => {
-		dispatch(statSlise.actions.statSelectCountry(country));
-	}, [data]);
 
 	return (
 		<div className="GeneralInformation">
