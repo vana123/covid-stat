690d6a5d9c2a262b75ebb9c1b06d41c8961ca19c
Vaana <35535603+vana123@users.noreply.github.com>
2022-06-21 10:22:24 +0300

    BarCharts display data with StatFilterReducer

--- src/components/BarCharts.tsx
+++ src/components/BarCharts.tsx
@@ -11,7 +11,6 @@ import {
 import { Bar } from "react-chartjs-2";
 import { useAppSelector, useAppDispatch } from "../hooks/redux";
 import { useEffect } from "react";
-import { IStat } from "../types/stat";
 import { useGetStatQuery } from "../service/statService";
 import { statFilterSlice } from "../store/reducers/StatFirlter";
 
@@ -37,31 +36,23 @@ export const options = {
 	},
 };
 
-function filterCountru(arr: IStat[], country: string) {
-	return arr.filter((item) => {
-		return item.countryRegion.toLowerCase().includes(country.toLowerCase());
-	});
-}
-
 export const BarCharts = () => {
 	const dispatch = useAppDispatch();
 	const { date } = useAppSelector((state) => state.dateReducer);
 	const { isError, isLoading, data } = useGetStatQuery(date);
 	const { country } = useAppSelector((state) => state.countryReducer);
-	let dataFiltre = data ? filterCountru(data, country).slice(0, 15) : [];
+	const { statData } = useAppSelector((state) => state.statFilterReducer);
 
 	useEffect(() => {
 		if (data) {
-			dataFiltre = filterCountru(data, country).slice(0, 15);
 			dispatch(statFilterSlice.actions.setStatData(data));
 			dispatch(statFilterSlice.actions.filterStatData({ data, country }));
 		} else {
-			dataFiltre = [];
 			dispatch(statFilterSlice.actions.setStatData([]));
 		}
 	}, [data, country]);
 
-	const labels = dataFiltre.map((item) => {
+	const labels = statData.map((item) => {
 		return item.countryRegion;
 	});
 
@@ -70,21 +61,21 @@ export const BarCharts = () => {
 		datasets: [
 			{
 				label: "confirmed",
-				data: dataFiltre.map((item) => {
+				data: statData.map((item) => {
 					return Number(item.confirmed);
 				}),
 				backgroundColor: "rgba(53, 162, 235, 0.5)",
 			},
 			{
 				label: "deaths",
-				data: dataFiltre.map((item) => {
+				data: statData.map((item) => {
 					return Number(item.deaths);
 				}),
 				backgroundColor: "rgba(255, 99, 132, 0.5)",
 			},
 			{
 				label: "recovered",
-				data: dataFiltre.map((item) => {
+				data: statData.map((item) => {
 					return Number(item.recovered);
 				}),
 				backgroundColor: "rgba(53, 120, 100, 0.5)",
